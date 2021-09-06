180effe21439711811d754ec6854cd956b2ac034
U-WSMTEC\yudekui <yudekui@pa-ops-pc005.wsmtec.ad>
2021-09-06 16:16:11 +0800

    test

---
+++ project/ssh/ssh_login.py
@@ -0,0 +1,28 @@
+﻿import paramiko
+import os
+
+def exp(host,port,user,password):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect(host,port,user,password,timeout=5)
+        ssh.close()
+        return True
+    except:
+        return False
+
+
+ if __name__ == "__main__":
+    host = args[1]
+    port = int(args[2])
+    user = args[3]
+    password = args[4]
+    if exp(host,port):
+        print("登录成功！")
+    else:
+        print("登录失败！")
+        
+
+    
+
+
