3f206bfd820ee4df0182f3c8935a59239f099c1b
U-WSMTEC\yudekui <yudekui@pa-ops-pc005.wsmtec.ad>
2021-09-07 10:49:54 +0800

    shiro

--- project/shiro/run.sh
+++ project/shiro/run.sh
@@ -1,2 +1,2 @@
-docker build -t shiro .
-docker run --rm shiro https://admin.mysiteforme.com/login
+docker build -t och-shiro .
+docker run --rm och-shiro shiro.py https://admin.mysiteforme.com/login
